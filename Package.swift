f33f2f9fded756d91c6abb5b2bdaba3da8fc0f95
Raffaele <raffa.darco@hotmail.it>
2025-09-18 16:40:37 +0200

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "DepOne",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "DepOne",
+            targets: ["DepOne"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DepOne",
+            url: "https://infocert.jfrog.io/artifactory/swift-snapshot-local/DepOne/1.0.0/DepOne.xcframework.zip"
+            checksum: "46ac5a33259fa78c57fa0d2df3d3bdf48cbad40b7be6ab19025176e3fe35ffe0"
+
+
+        )
+    ]
+)
