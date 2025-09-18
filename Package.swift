b060eef53f375a9b6867f839298e9d201372204c
Raffaele <raffa.darco@hotmail.it>
2025-09-18 17:46:33 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "DepOne",
             url: "https://infocert.jfrog.io/artifactory/swift-snapshot-local/DepOne/1.0.0/DepOne.xcframework.zip",
-            checksum: "46ac5a33259fa78c57fa0d2df3d3bdf48cbad40b7be6ab19025176e3fe35ffe0"
+            checksum: "1f84843c5fd887e93278f479423a2b77bc4230ef961763745ed8d463a896fb52"
         )
     ]
 )
